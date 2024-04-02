badec00e35509f42cdac93bd6c2fe32518dc7262
Eduardo Souza Ribeiro <er6451073@gmail.com>
2024-04-02 10:06:25 -0300

    atualizacao

--- RPG.1/attribute.py
+++ RPG.1/attribute.py
@@ -58,101 +58,101 @@ class Attribute:
     def definition_strength(self,strength):
         self.__strength = strength
         if strength == 0:
-            return strength, print("Incorpório")
+            return print("Incorpório: ",strength)
         elif strength >= 1 and strength <= 5 :
-            return strength, print("Incapaz")
+            return print("Incapaz: ",strength)
         elif strength >=6 and strength <=9:
-            return strength, print("Fraco")
+            return print("Fraco: ",strength)
         elif strength >=10 and strength <=11:
-            return strength, print("Medino")
+            return print("Medino: ",strength)
         elif strength >=12 and strength <=15:
-            return strength, print("Forte")
+            return print("Forte: ",strength)
         elif strength >=16 and strength <=20:
-            return strength, print("Super poderoso")
+            return print("Super poderoso: ",strength)
         else:
-            return strength, print("Inbatível")
+            return print("Inbatível: ",strength)
 
     def definition_dexterity(self, dexterity):
         self.__dexterity = dexterity
         if dexterity ==0:
-            return dexterity, print("Desacordado")
+            return print("Desacordado: ",dexterity)
         elif dexterity >=1 and dexterity <=5:
-            return dexterity, print("Abatido")
+            return print("Abatido: ",dexterity)
         elif dexterity >=6 and dexterity <=9:
-            return dexterity, print("Desajeitado")
+            return print("Desajeitado: ",dexterity)
         elif dexterity >=10 and dexterity <=11:
-            return dexterity, print("Regular")
+            return print("Regular: ",dexterity)
         elif dexterity >=12 and dexterity <=15:
-            return dexterity, print("Ágil")
+            return print("Ágil: ",dexterity)
         elif dexterity >=16 and dexterity <=20:
-            return dexterity, print("Ninja")
+            return print("Ninja: ")
         else:
-            return dexterity, print("Inperceptível") 
+            return print("Inperceptível: ",dexterity) 
     
     def definition_constitution(self,constitution):
         self.__constitution == constitution
         if constitution ==0:
-            return constitution, print("Espectro")
+            return print("Espectro: ",constitution)
         elif constitution >=1 and constitution <=5:
-            return constitution, print("Enfermo")
+            return print("Enfermo: ",constitution)
         elif constitution >=6 and constitution <=9:
-            return constitution, print("Frágio")
+            return print("Frági: ",constitution)
         elif constitution >=10 and constitution <=11:
-            return constitution, print("Saudável")
+            return print("Saudável: ",constitution)
         elif constitution >=12 and constitution <=15:
-            return constitution, print("Durão")
+            return print("Durão: ",constitution)
         elif constitution >=16 and constitution <=20:
-            return constitution, print("Super resistênte")
+            return print("Super resistênte: ",constitution)
         else:
-            return constitution, print("Imortal")
+            return print("Imortal: ",constitution)
     
     def definition_wisdom(self,wisdom):
         self.__wisdom == wisdom
         if wisdom ==0:
-            return wisdom, print("Inconscinte")
+            return print("Inconscinte: ",wisdom)
         elif wisdom >=1 and wisdom <=5:
-            return wisdom, print("Irracional")
+            return print("Irracional: ",wisdom)
         elif wisdom >=6 and wisdom <=9:
-            return wisdom, print("Desatento")
+            return print("Desatento: ",wisdom)
         elif wisdom >=10 and wisdom <=11:
-            return wisdom, print("Simples")
+            return print("Simples: ",wisdom)
         elif wisdom >=12 and wisdom <=15:
-            return wisdom, print("Perspcaz")
+            return print("Perspcaz: ",wisdom)
         elif wisdom >=16 and wisdom <=20:
-            return wisdom, print("Filósofo")
+            return print("Filósofo: ",wisdom)
         else:
-            return wisdom, print("Iluminado")
+            return print("Iluminado: ",wisdom)
 
     def definition_intelligence(self,intelligence):
         self.__intelligence == intelligence
         if intelligence ==0:
-            return intelligence, print("Inanimado")
+            return print("Inanimado: ",intelligence)
         elif intelligence >=1 and intelligence <=5:
-            return intelligence, print("Incivilizado")
+            return print("Incivilizado: ",intelligence)
         elif intelligence >=6 and intelligence <=9:
-            return intelligence, print("Parvo")
+            return print("Parvo: ",intelligence)
         elif intelligence >=10 and intelligence <=11:
-            return intelligence, print("Medíocre")
+            return print("Medíocre: ",intelligence)
         elif intelligence >=12 and intelligence <=15:
-            return intelligence, print("Estuado")
+            return print("Estuado: ",intelligence)
         elif intelligence >=16 and intelligence <=20:
-            return intelligence, print("Gênio")
+            return print("Gênio: ",intelligence)
         else:
-            return intelligence, print("Onisciente")
+            return print("Onisciente: ",intelligence)
 
     def definition_charisma(self,charisma):
         self.__charisma == charisma
         if charisma ==0:
-            return charisma, print("Aberração")
+            return print("Aberração: ",charisma)
         elif charisma >=1 and charisma <=5:
-            return charisma, print("Inexprecivo")
+            return print("Inexprecivo: ",charisma)
         elif charisma >=6 and charisma <=9:
-            return charisma, print("Rude")
+            return print("Rude: ",charisma)
         elif charisma >=10 and charisma <=11:
-            return charisma, print("Socíavel")
+            return print("Socíavel: ",charisma)
         elif charisma >=12 and charisma <=15:
-            return charisma, print("Persuasivo")
+            return print("Persuasivo: ",charisma)
         elif charisma >=16 and charisma <=20:
-            return charisma, print("Influente")
+            return print("Influente: ",charisma)
         else:
-            return charisma, print("Ídolo")
+            return print("Ídolo: ",charisma)
